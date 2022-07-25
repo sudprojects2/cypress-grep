db6962002408e87eba9c74b482c5c150f31db7ee
sudprojects2 <sudhaan80@gmail.com>
2022-07-25 14:46:45 -0400

    test1

--- cypress/integration/burn-spec.js
+++ cypress/integration/burn-spec.js
@@ -1,7 +1,7 @@
 /// <reference types="cypress" />
 
 // if we specify just the burn parameter
-// then this test will be repeated N times
+// then this test will be repeated N times test
 describe('burning a test N times', () => {
   it('repeats', () => {})
 
